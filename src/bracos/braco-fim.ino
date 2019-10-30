46c4e6da1707575c63b4173a5e42fed55f41f28d
Sara Ferreira <ferreirasara1501@hotmail.com>
2019-10-30 19:23:26 -0300

    Definição e inicialização dos pinos para os servos e os eletroímãs.

---
+++ src/bracos/braco-fim.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+#include "defines.h"
+
+void setup() {
+    // Inicializacao dos servos
+    pinMode(pinoServoBase, OUTPUT);
+    pinMode(pinoServo1, OUTPUT);
+    pinMode(pinoServo2, OUTPUT);
+
+    // Inicializacao do eletroima
+    pinMode(pinoEletroIma, OUTPUT);
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+
+}
---
+++ src/bracos/defines.h
@@ -0,0 +1,9 @@
+// Declaracao do pino que controlara o servo da base do braco
+#define pinoServoBase
+
+// Declaracao dos pinos dos outros pinoServoBase
+#define pinoServo1
+#define pinoServo2
+
+// Declaracao do pino para o eletroima
+#define pinoEletroIma
