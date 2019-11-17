c1933b9c4d87081a60ff6980cb54bbacbbdf2eab
Sara Ferreira <ferreirasara1501@hotmail.com>
2019-11-17 09:27:23 -0300

    Melhoria da estrutura de pastas.

---
+++ src/bracos/braco-inicio/defines.h
@@ -0,0 +1,19 @@
+// Declaracao do pino que controlara o servo da base do braco
+#define pinoServoBase
+
+// Declaracao dos pinos dos outros pinoServoBase
+#define pinoServo1
+#define pinoServo2
+
+// Declaracao do pino para o eletroima
+#define pinoEletroIma
+
+// Declaracao do pino do sensor, para saber quando o robo esta parado perto do braco
+// Sera um sensor infravermelho
+#define pinoSensor
+
+// Declaracao das variaveis para a posicao de cada servo
+// A posicao varia de 0 a 360 (uma volta completa)
+int posicaoServoBase;
+int posicaoServo1;
+int posicaoServo2;
