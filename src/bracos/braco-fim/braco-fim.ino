dd117dd730603495cb6383514cb804c8810bd7db
Sara Ferreira <ferreirasara1501@hotmail.com>
2019-11-20 21:47:03 -0300

    Adicionadas funções para mover os servos.

--- src/bracos/braco-fim/braco-fim.ino
+++ src/bracos/braco-fim/braco-fim.ino
@@ -12,7 +12,7 @@ void setup() {
 	servo1.attach(pinoServo1);
 	servo2.attach(pinoServo2);
 
-	// Inicia os servos na posicao 0
+	// Inicia os servos na posicao 0 (posicao inicial)
 	servoBase.write(0);
 	servo1.write(0);
 	servo2.write(0);
@@ -28,23 +28,31 @@ void setup() {
 void loop() {
     if (digitalRead(pinoSensor) == LOW) { // Verifica se o robo esta parado ao lado do braco
         // Nos testes, o sensor manda um sinal LOW quando o objeto esta proximo
+        // OBS: Precisa ajustar a "sensibilidade" do sensor, de modo que detecte somente o robo
         // Abaixa o braco para chegar perto do cubo
+        posicaoServo1 = 0; // Verificar qual sera o angulo
+        servoBase.write(posicaoServoBase);
 
         // Liga o eletroima
         digitalWrite(pinoEletroIma, HIGH);
 
         // Ergue novamente o braco
+        posicaoServo1 = 0; // Verificar qual sera o angulo
+        servoBase.write(posicaoServoBase);
 
         // Move o braco para a esquerda (deixar em cima do robo)
-        posicaoServoBase = 180; // Verificar qual sera o angulo
-        servoBase.write(posicaoServoBase)
+        posicaoServoBase = 0; // Verificar qual sera o angulo
+        servoBase.write(posicaoServoBase);
 
         // Abaixa o braco
+        posicaoServo1 = 0; // Verificar qual sera o angulo
+        servoBase.write(posicaoServoBase);
 
         // Desliga o eletroima
         digitalWrite(pinoEletroIma, LOW);
 
         // Ergue novamente o braco
-
+        posicaoServo1 = 0; // Verificar qual sera o angulo
+        servoBase.write(posicaoServoBase);
     }
 }
--- src/bracos/braco-inicio/braco-inicio.ino
+++ src/bracos/braco-inicio/braco-inicio.ino
@@ -12,38 +12,47 @@ void setup() {
 	servo1.attach(pinoServo1);
 	servo2.attach(pinoServo2);
 
-	// Inicia os servos na posicao 0
+	// Inicia os servos na posicao 0 (posicao inicial)
 	servoBase.write(0);
 	servo1.write(0);
 	servo2.write(0);
 
 	// Inicializacao do eletroima
     pinMode(pinoEletroIma, OUTPUT);
+    digitalWrite(pinoEletroIma, LOW);
 
-	// Inicializacao do sensor
-	pinMode(pinoSensor, INPUT_PULLUP); // Usa PULLUP para nao ter variacao entre 0 e 1
+    // Inicializacao do sensor
+	pinMode(pinoSensor, INPUT);
 }
 
 void loop() {
-	if (digitalRead(pinoSensor) == LOW) { // Verifica se o robo esta parado ao lado do braco
+    if (digitalRead(pinoSensor) == LOW) { // Verifica se o robo esta parado ao lado do braco
         // Nos testes, o sensor manda um sinal LOW quando o objeto esta proximo
+        // OBS: Precisa ajustar a "sensibilidade" do sensor, de modo que detecte somente o robo
         // Abaixa o braco para chegar perto do cubo
+        posicaoServo1 = 0; // Verificar qual sera o angulo
+        servoBase.write(posicaoServoBase);
 
         // Liga o eletroima
         digitalWrite(pinoEletroIma, HIGH);
 
         // Ergue novamente o braco
+        posicaoServo1 = 0; // Verificar qual sera o angulo
+        servoBase.write(posicaoServoBase);
 
-        // Move o braco para a direita (deixar em cima da plataforma)
-        posicaoServoBase = 180; // Verificar qual sera o angulo
-        servoBase.write(posicaoServoBase)
+        // Move o braco para a esquerda (deixar em cima do robo)
+        posicaoServoBase = 0; // Verificar qual sera o angulo
+        servoBase.write(posicaoServoBase);
 
         // Abaixa o braco
+        posicaoServo1 = 0; // Verificar qual sera o angulo
+        servoBase.write(posicaoServoBase);
 
         // Desliga o eletroima
         digitalWrite(pinoEletroIma, LOW);
 
         // Ergue novamente o braco
-
+        posicaoServo1 = 0; // Verificar qual sera o angulo
+        servoBase.write(posicaoServoBase);
     }
 }
